1ccb483e2eda11171e8c5409003b0ce9fbd4b54d
sadiqkhan123 <sk3567273@gmail.com>
2023-07-12 16:26:51 +0500

    print names from list

---
+++ Q1.dart
@@ -0,0 +1,110 @@
+void main() {
+  //Q1
+  List names =["Sadiq","Ameen","Momin","Basit"];
+  print(names);
+}  
+//Q10
+// List <String> names = ["Bilal","Ali","Shahreef","Qasim","Ali","Fardeen","Bilal"];
+// List <String> noDuplicateElements = [];
+// names.forEach((element) { 
+// if(!noDuplicateElements.contains(element))
+// {
+//   noDuplicateElements.add(element);
+// }
+// });
+// print(noDuplicateElements);
+// }
+//Q11
+// // List <int> numbers =[9,8,6,5,4,3,40,77];
+// // int n =5;
+// // List <int> newList=numbers.sublist(0,n);
+// // print(newList);
+//Q12
+//  List <int> numbers =[9,8,6,5,4,3,40,77];
+//  print(numbers);
+//  var list = List.of(numbers.reversed);
+//  print(list);
+//Q13
+// List<int> num =[1,4,7,9,5,8,6,4,1,0];
+// List <int> unique =[];
+// for(var element in num)
+// {
+//   if(!unique.contains(element))
+//   {
+//     unique.add(element);
+    
+//   }
+// }
+// print(unique);
+//Q14
+//  List <int> numbers =[9,8,6,5,4,3,40,77];
+//  print(numbers);
+//  numbers.sort();
+//  List <int> newlist =numbers;
+//  print(newlist);
+//Q15
+//  List <int> numbers =[9,8,-6,5,4,-3,-40,77];
+//  print(numbers);
+//  Iterable <int> filterout =List.of(numbers.where((element) =>element>0));
+//  print(filterout);
+// Q16 
+//  List <int> numbers =[9,8,6,5,4,3,40,77];
+//  print(numbers);
+//  Iterable <int> filterout =List.of(numbers.where((elements)=>elements%2==0));
+//  print(filterout);
+//Q17
+//  List <int> numbers =[9,8,6,5,4,3,40,77];
+//  print(numbers);
+//  Iterable <int> filterout =List.of(numbers.map((elements)=>elements*elements));
+//  print(filterout);
+//Q18
+// Map <String ,dynamic> person = {"name":"John","Age":18,"isStudent":true};
+// if(person["student"]==true && person["Age"]>=18)
+// {
+//   print("${person["name"]} is Eligible");
+// }
+// else
+// {
+//    print("${person["name"]} is Eligible");
+// }
+//Q19
+// Map <String ,dynamic> Product ={"name":"Ketchup","Price":550,"Quantity":3};
+// if(Product["Quantity"]>=0)
+// {
+//   print("${Product["name"]} is in stock");
+// }
+// else
+// {
+//   print("${Product["name"]} out of stock");
+// }
+//Q20
+// Map car ={"brand":"Toyota","Color":"red","isSedden":true};
+// if(car["Color"]=="red"&&car["isSedden"]==true)
+// {
+//   print("Match");
+// }
+// else
+// {
+//   print("Not Match");
+// }
+//Q21
+// Map user ={"name":"Sadiq","Admin":true,"Active":true};
+// if(user["Admin"]==true &&user["Active"]==true)
+// {
+//   print("is Active");
+// }
+// else
+// {
+//   print("is not active");
+// }
+//Q22
+// Map ShopppingCart  ={"ProductName":"Apple","Quantities":3};
+// if(ShopppingCart["ProductName"]=="Apple")
+// {
+//   print("Product found");
+// }
+// else
+// {
+//   print("Product not found");
+// }
+
